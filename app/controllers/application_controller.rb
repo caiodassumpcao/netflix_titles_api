d5829a7e842e168c2e6297343651108dfef98b84
caiodassumpcao <caio.rodri.d@gmail.com>
2023-11-13 09:24:26 -0300

    Feat application_controller.rb: Configure controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+  private
+
+  def render_nout_found
+    render json: { error: 'Not Found' }, status: :not_found
+  end
 end
+class NetflixTitlesController < ApplicationController
+
+end
+
