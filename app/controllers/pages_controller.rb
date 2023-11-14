9fe68aaa7caab11851f5b4bdb5aa50020a3a92e5
caiodassumpcao <caio.rodri.d@gmail.com>
2023-11-14 10:34:03 -0300

    Fix application_controller.rb: Configure controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
   def index
+    # Nenhuma lógica necessária para a página inicial por enquanto
   end
 
+  # @return [nil]
   def list
+    @netflix_titles = NetflixTitle.order(year: :asc)
+    render 'list'
   end
 end
